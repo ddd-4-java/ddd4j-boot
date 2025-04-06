d58d2caa09288c42e26809e98a3f2ab7da0a7af2
luffy <luffy@uzx.com>
2025-04-07 01:40:27 +0800

    feat： mqtt

---
+++ jeebiz-boot-samples/jeebiz-boot-sample-druid-mqtt-server/src/main/java/io/hiwepy/boot/sample/setup/listener/MqttServerMessageListener.java
@@ -0,0 +1,21 @@
+package io.hiwepy.boot.sample.setup.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.mica.mqtt.codec.MqttPublishMessage;
+import org.dromara.mica.mqtt.codec.MqttQoS;
+import org.dromara.mica.mqtt.core.server.event.IMqttMessageListener;
+import org.springframework.stereotype.Service;
+import org.tio.core.ChannelContext;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+@Slf4j
+public class MqttServerMessageListener implements IMqttMessageListener {
+
+    @Override
+    public void onMessage(ChannelContext context, String clientId, String topic, MqttQoS qoS, MqttPublishMessage message) {
+        log.info("clientId:{} message:{} payload:{}", clientId, message, new String(message.getPayload(), StandardCharsets.UTF_8));
+    }
+
+}
