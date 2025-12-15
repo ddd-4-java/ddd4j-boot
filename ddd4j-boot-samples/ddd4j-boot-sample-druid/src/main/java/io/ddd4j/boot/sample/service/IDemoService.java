1aa244c176d1dca1f2ccf731acda70e21264a630
wandl-6A72h <hiwepy@gmail.com>
2025-12-15 12:04:16 +0800

    feat(samples): 添加多个示例子模块
    - 新增 ddd4j-boot-sample-adapter 模块
    - 新增 ddd4j-boot-sample-app 模块
    - 新增 ddd4j-boot-sample-client 模块
    - 新增 ddd4j-boot-sample-common 模块
    - 新增 ddd4j-boot-sample-domain 模块
    - 新增 ddd4j-boot-sample-infrastructure 模块
    - 在父 pom 中注册所有新模块
    - 配置各模块的基础 Maven 属性
    - 为 common 模块添加 COLA 组件依赖

---
+++ ddd4j-boot-samples/ddd4j-boot-sample-druid/src/main/java/io/ddd4j/boot/sample/service/IDemoService.java
@@ -0,0 +1,16 @@
+package io.ddd4j.boot.sample.service;
+
+import io.ddd4j.boot.core.service.IBaseService;
+import io.ddd4j.boot.sample.demo.infra.persistence.entity.DemoEntity;
+
+/**
+ * <p>
+ * Demo示例表 服务类
+ * </p>
+ *
+ * @author wandl
+ * @since 2023-08-06
+ */
+public interface IDemoService extends IBaseService<DemoEntity> {
+
+}
