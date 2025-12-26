305cd62d868fee470e4d23f1f82f8a4a6e568411
wandl-6A72h <hiwepy@gmail.com>
2025-12-26 10:13:26 +0800

    feat(utils): 添加类工具和国际化消息工具类
    - 将AppUtils中的SERVER_NAME常量从jeebiz_2021更新为ddd4j_2021
    - 新增ClassUtils工具类，提供方法参数获取和注解处理功能
    - 新增MessagesUtils工具类，提供国际化消息获取功能
    - 实现方法参数信息获取功能，支持构造器和方法参数处理
    - 实现注解获取功能，支持方法和类级别的注解查找
    - 集成Spring的参数名发现器和桥接方法解析器
    - 提供多语言消息获取的便捷方法

--- ddd4j-boot-core/src/main/java/io/ddd4j/boot/core/util/AppUtils.java
+++ ddd4j-boot-core/src/main/java/io/ddd4j/boot/core/util/AppUtils.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class AppUtils {
 
     //生成 app_secret 密钥
-    private final static String SERVER_NAME = "jeebiz_2021";
+    private final static String SERVER_NAME = "ddd4j_2021";
     private final static String[] chars = new String[]{"a", "b", "c", "d", "e", "f",
             "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
             "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
