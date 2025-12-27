1c597f8cc654241d28778c995e09588bed8ad812
wandl-6A72h <hiwepy@gmail.com>
2025-12-27 17:35:11 +0800

    chore(samples): 移除ActiveMQ相关配置和控制器文件
    - 删除ddd4j-boot-sample-starter-druid-activemq模块中的ActiveMQConfiguration配置类
    - 删除ddd4j-boot-sample-starter-hikaricp-activemq模块中的ActiveMQConfiguration配置类
    - 删除ddd4j-boot-sample-starter-druid-activemq模块中的ActiveMQController控制器
    - 删除ddd4j-boot-sample-starter-hikaricp-activemq模块中的ActiveMQController控制器
    - 移除两个模块中的application.properties配置文件
    - 移除两个模块中的application.yaml配置文件
    -

---
+++ ddd4j-boot-samples/ddd4j-boot-sample-starter-druid-mqtt-server/src/main/java/io/hiwepy/boot/sample/exception/BizExceptionHandler.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2018 Hiwepy (http://hiwepy.io).
+ * All Rights Reserved.
+ */
+package io.hiwepy.boot.sample.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class BizExceptionHandler {
+
+    /**
+     * 500 (降级熔断)
+     @ExceptionHandler({ ClientException.class })
+     @ResponseBody public ResponseEntity<ApiRestResponse<String>> netflixClientException(ClientException ex) {
+     this.logException(ex);
+     return new ResponseEntity<>( BizExceptionCode.SYSTEM_DEPEND_UPGRADING.asResponse(messageSource), HttpStatus.OK);
+     }
+     */
+
+}
